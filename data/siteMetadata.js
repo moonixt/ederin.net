c6ff0dc1c5405300031b632f1052032c5d659fdf
Derek <derekoob@hotmail.com>
2025-02-20 00:44:36 -0300

    1.0

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,10 +6,10 @@ const siteMetadata = {
   description: 'Criado com Next.js, Tailwind CSS e MDX',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://tailwind-css-starter-blog-orpin-gamma.vercel.app/',
+  siteRepo: 'https://github.com/moonixt/tailwind-weblog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/dd.jpg`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com/moonixt',
